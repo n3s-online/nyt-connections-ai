ebaeb35a60400360e8a89f3b8f7e8630c0b1e89f
n3s-online <n3s.online@gmail.com>
2023-10-16 21:34:47 -0700

    set up environment variables

--- src/main.py
+++ src/main.py
@@ -5,13 +5,18 @@
 import time
 from selenium import webdriver
 
+from dotenv import load_dotenv
+import os
+
 from connections import Connections
 from game import Game
 
+# Environment variables
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Choose Chrome Browser
 browser = webdriver.Chrome()
-
 connections = Connections(browser, 100)
 
 # Create a new game
